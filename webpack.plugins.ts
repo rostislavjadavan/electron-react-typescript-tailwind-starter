90b1261f8e670c8f69c6d41b35674b436f3880c7
Rostislav Jadavan <rostislav@jadavan.com>
2025-09-03 20:25:19 +0200

    removed fork-ts-checker-webpack-plugin

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -1,10 +1,3 @@
-import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import type { WebpackPluginInstance } from 'webpack';
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-
-export const plugins = [
-  new ForkTsCheckerWebpackPlugin({
-    logger: 'webpack-infrastructure',
-  }),
-];
+export const plugins: WebpackPluginInstance[] = [];
